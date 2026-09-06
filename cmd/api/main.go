0af72d741d426a869d7bce7753be253b538660e2
samrwitt <samrawitkta@gmail.com>
2026-09-06 17:56:07 +0300

    get /code

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -20,10 +22,29 @@ type ShortenResponse struct {
 	ShortUrl string `json:"short_url"`
 }
 
+func generateShortCode(length int) (string, error) {
+	const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
+	result := make([]byte, length)
+
+	for i := range result {
+		randomByte := make([]byte, 1)
+
+		if _, err := rand.Read(randomByte); err != nil{
+			return "", err
+		}
+
+		result[i] = chars[int(randomByte[0])%len(chars)]
+	}
+
+	return string(result), nil
+}
 
 func main() {
 	router := chi.NewRouter()
+
+	urls := make(map[string]string)
+	var mu sync.RWMutex
 
 	router.Get("/health", func(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusOK)
@@ -39,10 +60,19 @@ func main() {
 			return
 		}
 
+		shortCode, err := generateShortCode(6)
+		if err != nil{
+			http.Error(w, "failed to generate short code", http.StatusInternalServerError)
+			return
+		}
+		mu.Lock()
+		urls[shortCode] = input.URL
+		mu.Unlock()
+
 		response := ShortenResponse{
 			OriginalUrl: input.URL,
-			ShortCode:   "abc123",
-			ShortUrl:    "http://localhost:8080/abc123",
+			ShortCode:   "shortCode",
+			ShortUrl:    "http://localhost:8080/" + shortCode,
 		}
 
 		w.Header().Set("Content-Type", "application/json")
@@ -51,6 +81,21 @@ func main() {
 		json.NewEncoder(w).Encode(response)
 	})
 
+
+	router.Get(("/code"), func(w http.RequestWriter, req *httpRequest){
+		shortCode := chi.URLParam(req, "code")
+
+		mu.Rlock()
+		originalURL, exists := urls[shortCode]
+		mu.RUnlock()
+
+		if !exists(
+			http.Error(w, "short URL not found", http.StatusNotFound)
+			return
+		)
+
+		http.Redirect(w, req, originalURL, http.StatusFound)
+	})
 
 	log.Println("server running on :8080")
 
